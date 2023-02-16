18decf769c089dcc2c69044f9e12b875b7fd6a61
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2023-02-16 11:57:45 +0200

    Importing emis

--- emis/src/main/java/org/saudigitus/emis/ui/attendance/components/Toolbar.kt
+++ emis/src/main/java/org/saudigitus/emis/ui/attendance/components/Toolbar.kt
@@ -16,7 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
-import org.saudigitus.emiscapture.R
+import org.saudigitus.emis.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
