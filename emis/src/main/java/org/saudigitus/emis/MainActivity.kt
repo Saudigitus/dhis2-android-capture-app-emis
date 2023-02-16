a95d817dead4f8849393d6d879ea5d8f3bae2fc0
Carlos Macaneta <carlosmacaneta@hotmail.com>
2023-02-16 14:52:55 +0200

    implementing attendance screen view

--- emis/src/main/java/org/saudigitus/emis/MainActivity.kt
+++ emis/src/main/java/org/saudigitus/emis/MainActivity.kt
@@ -10,21 +10,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import org.saudigitus.emis.ui.attendance.AttendanceScreen
 import org.saudigitus.emis.ui.theme.EmisCaptureTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            EmisCaptureTheme {
+            //EmisCaptureTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AttendanceScreen()
                 }
-            }
+            //}
         }
     }
 }
